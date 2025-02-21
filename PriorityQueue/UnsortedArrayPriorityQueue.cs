2405929aa2781fc2cb7afed9e1070b33c427691f
A-Macleod <0308180@uhi.ac.uk>
2025-02-21 03:04:27 +0000

    UnsortedArrayPriorityQueue / Created class with private fields and constructor
    The constructor sets the _tailIndex = -1 on creation of a new UnsortedArrayPriorityQueue. This is how we track that the array is empty and also how we track the tail index to check if the array is full.

--- PriorityQueue/UnsortedArrayPriorityQueue.cs
+++ PriorityQueue/UnsortedArrayPriorityQueue.cs
@@ -6,28 +6,33 @@ using System.Threading.Tasks;
 
 namespace PriorityQueue
 {
-    //class UnsortedArrayPriorityQueue<T> : PriorityQueue<T>
-    //{
-    //    private readonly PriorityItem<T>[] storage;
-    //private readonly 
+
+    /// <summary>
+    /// Class to represent an unsorted, array based, priority queue.
+    /// Inheriting members from the PriorityQueue Interface.
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
+    class UnsortedArrayPriorityQueue<T> : PriorityQueue<T>
+    {
+
+        /// <summary>
+        /// Private fields for unsorted array priority queue
+        /// </summary>
+        private readonly PriorityItem<T>[] _storage;
+        private readonly int _capacity;
+        private int _tailIndex;
 
 
-
-
-
-    //void Add(T item, int priority)
-    //{ }
-
-    //T Head()
-    //{ }
-
-    //void Remove()
-    //{ }
-
-    //bool IsEmpty()
-    //{ }
-
-    //string ToString()
-    //{ }
-   // }
+        /// <summary>
+        /// Constructor for unsorted array priority queue that takes in a size arguement
+        /// to determine the size of the array we want to create
+        /// </summary>
+        /// <param name="size">The array size</param>
+        public UnsortedArrayPriorityQueue(int size)
+        {
+            _storage = new PriorityItem<T>[size]; 
+            _capacity = size;
+            _tailIndex = -1;
+        }
+    }
 }
