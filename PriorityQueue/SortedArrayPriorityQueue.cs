77f339c8384df6b41e800db0cf682ad88c1424f5
A-Macleod <0308180@uhi.ac.uk>
2025-02-21 02:17:18 +0000

    SortedArrayPriorityQueue / Added code comments to class, fields, methods with descriptions

--- PriorityQueue/SortedArrayPriorityQueue.cs
+++ PriorityQueue/SortedArrayPriorityQueue.cs
@@ -2,12 +2,27 @@
 
 namespace PriorityQueue
 {
+
+    /// <summary>
+    /// Class to represent a sorted, array based, priority queue
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
     public class SortedArrayPriorityQueue<T> : PriorityQueue<T>
     {
+
+        /// <summary>
+        /// Private fields for sorted array priority queue
+        /// </summary>
         private readonly PriorityItem<T>[] storage;
         private readonly int capacity;
         private int tailIndex;
 
+
+        /// <summary>
+        /// Constructor for sorted array priority queue that takes in a size argument
+        /// to determine the size of the array
+        /// </summary>
+        /// <param name="size">The array size</param>
         public SortedArrayPriorityQueue(int size)
         {
             storage = new PriorityItem<T>[size];
@@ -15,6 +30,12 @@ namespace PriorityQueue
             tailIndex = -1;
         }
 
+
+        /// <summary>
+        /// Method to return the head of the array, at index 0, and the Item data
+        /// </summary>
+        /// <returns>Index position 0 item data</returns>
+        /// <exception cref="QueueUnderflowException"></exception>
         public T Head()
         {
             if (IsEmpty())
@@ -24,6 +45,17 @@ namespace PriorityQueue
             return storage[0].Item;
         }
 
+
+        /// <summary>
+        /// Method to add an item and priority to the sorted array priority queue. Increments
+        /// the tailIndex by 1 and checks if the size of the array is greater or equal to the
+        /// capacity. If so, throws an exception. Starting at the tail index, check if the previous 
+        /// Index priority is less than the incoming priority, if so move lower priority item to the right.
+        /// The new PriorityItem is now inserted into this space. 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="priority"></param>
+        /// <exception cref="QueueOverflowException"></exception>
         public void Add(T item, int priority)
         {
             tailIndex++;
@@ -42,6 +74,13 @@ namespace PriorityQueue
             storage[i] = new PriorityItem<T>(item, priority);
         }
 
+
+        /// <summary>
+        /// Method to check if array tailIndex is less than 0. If so thow UnderflowException.
+        /// Iterates through the array and copys and moves each Index one to the left. Then
+        /// removes the tail index item from the array
+        /// </summary>
+        /// <exception cref="QueueUnderflowException"></exception>
         public void Remove()
         {
             if (IsEmpty())
@@ -56,11 +95,25 @@ namespace PriorityQueue
             tailIndex--;
         }
 
+
+        /// <summary>
+        /// Method to return true if the tailIndex is less than 0. This shows us the array is empty
+        /// as each newly added item increases the tail by 1 index. The first item will be index 0.
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
             return tailIndex < 0;
         }
 
+
+        /// <summary>
+        /// Method to override the ToString method and throw an exception if the array is empty (tailIndex is less than 0).
+        /// Adds a bracket infront and after each returned array index value, seperated by an added comma in the nested loop.
+        /// Returns each index value in the array, the Item and Priority.
+        /// </summary>
+        /// <returns>Array index value</returns>
+        /// <exception cref="QueueUnderflowException"></exception>
         public override string ToString()
         {
             if (IsEmpty())
--- PriorityQueue/UnsortedArrayPriorityQueue.cs
+++ PriorityQueue/UnsortedArrayPriorityQueue.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace PriorityQueue
 {
-    class UnsortedArrayPriorityQueue<T> : PriorityQueue<T>
-    {
-    }
+    //class UnsortedArrayPriorityQueue<T> : PriorityQueue<T>
+    //{
+    //    private readonly PriorityItem<T>[] storage;
+    //private readonly 
+
+
+
+
+
+    //void Add(T item, int priority)
+    //{ }
+
+    //T Head()
+    //{ }
+
+    //void Remove()
+    //{ }
+
+    //bool IsEmpty()
+    //{ }
+
+    //string ToString()
+    //{ }
+   // }
 }
