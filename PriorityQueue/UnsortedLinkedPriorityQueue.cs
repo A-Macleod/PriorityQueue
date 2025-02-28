e41c36a906f8164b4a6295707df9943805db1185
A-Macleod <0308180@uhi.ac.uk>
2025-02-28 01:49:59 +0000

    UnsortedLinkedPriorityQueue / Added Remove method with code commenting
    Method tracks the currentNode, highestPriorityNode and previousNode. Method compares the values of the Priorities for each Node in the LinkedList and keeps a track of the Highest Priority Node and the Previous Node. Removes the Highest Priority Node by changing the pointers of the Previous Nodes to the Node Next to the Highest Priority Node.

--- PriorityQueue/UnsortedLinkedPriorityQueue.cs
+++ PriorityQueue/UnsortedLinkedPriorityQueue.cs
@@ -10,8 +10,8 @@ namespace PriorityQueue
 {
     public class UnsortedLinkedPriorityQueue<T> : PriorityQueue<T> 
     {
-        private T _data;
-        private int _priority;
+        private readonly T _data;
+        private readonly int _priority;
         private Node<T> _head;
 
         public UnsortedLinkedPriorityQueue()
@@ -20,6 +20,14 @@ namespace PriorityQueue
         }
 
 
+        /// <summary>
+        /// UnsortedLinkedList method to create a newNode object with a generic data argument and priority.
+        /// Point newNode pointer to the current head, Update the head to point to the newNode.
+        /// This will add a newNode to the end of the LinkedList, without sorting, and pointing it to the
+        /// Node below it.
+        /// </summary>
+        /// <param name="data">Generic data type</param>
+        /// <param name="priority">Priority of data</param>
         public void Add(T data, int priority)
         {
             Node<T> newNode = new Node<T>(data, priority);  // create new node
@@ -29,10 +37,51 @@ namespace PriorityQueue
         }
 
 
+        /// <summary>
+        /// UnsortedLinkedList method to remove the Node with the Highest Priority by changing the Previous Nodes Pointer
+        /// to the Node after the Highest Priority Node. If there is no Nodes, throw exception error. Traverse the LinkedList 
+        /// searching for the Node with the Highest Priority, while comparing the Priority value against the Next node. 
+        /// Remove the HighestPriorityNode by changing the Pointers.  
+        /// </summary>
+        /// <exception cref="QueueUnderflowException">Exception error. There is no Nodes</exception>
+        public void Remove()
+        {
+            if (IsEmpty())
+            {
+                throw new QueueUnderflowException();
+            }
+
+            Node<T> currentNode = _head;
+            Node<T> highestPriorityNode = _head;
+            Node<T> previousNode = null;
+
+            // Traverse the LinkedList to find the Node with the Highest Priority, starting at the Head Node
+            while (currentNode != null)
+            {
+                if (currentNode.Priority > highestPriorityNode.Priority)    // Compare values
+                {
+                    highestPriorityNode = currentNode;                      // Update the new Highest Priority Node 
+                    previousNode = currentNode;
+                }
+                currentNode = currentNode.Next;                             // Move to the next Node
+            }
 
 
-
-
+            /* Removing the Highest Priority Node.
+            /  This jumps the link over the HighestPriorityNode because we change the Pointers,
+            /  we link the Previous Node to the one AFTER the HighestPriority using the
+            /  HighestPriorityNodes Next Pointer.   [Previous] [HighestPrio] [HighestPrioNext]
+            /                                           |____________________________|      
+            */
+            if (highestPriorityNode == _head)   // If the Node to be removed is the head
+            {
+                _head = _head.Next;             // Change the pointer of the head to the next Node in the LinkedList
+            }
+            else
+            {
+                previousNode.Next = highestPriorityNode.Next;   // Change the PreviousNodes Pointer to HighestPriorityNodes Next Pointer.
+            }  
+        }
 
 
         /// <summary>
@@ -44,14 +93,5 @@ namespace PriorityQueue
         {
             return _head == null;
         }
-
-
-
-
-
-
-
-
-
     }
 }
