c23d84d8ae7a2268b1cd3af60635882f572bbda1
A-Macleod <0308180@uhi.ac.uk>
2025-03-05 13:01:56 +0000

    UnsortedLinkedPriorityQueue / Added Second Remove Method
    Remove method traverses each Link keeping track of the count of the Nodes. The Highest Priority Node is then recorded. We Traverse the LinkedList a second time to find the Node previous to the HighestPriorityNode. Finally we update the pointers on the PreviousNode to point to the Node after the HighestPriorityNode. This skips over the HighestPriorityNode and removes it from the LinkedList.
    
    This is extremely inefficient as the time complexity is O(n)
    
    The second Remove method was created due to logic problems with the first Remove method.

--- PriorityQueue/UnsortedLinkedPriorityQueue.cs
+++ PriorityQueue/UnsortedLinkedPriorityQueue.cs
@@ -5,13 +5,13 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace PriorityQueue
 {
     public class UnsortedLinkedPriorityQueue<T> : PriorityQueue<T> 
     {
-        private T _item;
-        private int _priority;
         private Node<T> _head;
 
         public UnsortedLinkedPriorityQueue()
@@ -39,17 +39,15 @@ namespace PriorityQueue
                 }
                 currentNode = currentNode.Next;                             // Move to the next Node
             }
-
             return highestPriorityNode.Item;
-
         }
 
 
         /// <summary>
-        /// UnsortedLinkedList method to create a newNode object with a generic item argument and priority.
-        /// Point newNode pointer to the current head, Update the head to point to the newNode.
-        /// This will add a newNode to the end of the LinkedList, without sorting, and pointing it to the
-        /// Node below it.
+        /// UnsortedLinkedList method to create a newNode object with a generic item argument and integer priority.
+        /// Traverse the LinkedList until the end Node. Set the end Node Next pointer to this newNode. 
+        /// This will add the newNode to the end of the LinkedList. This will maintain the LIFO order of the LinkedList
+        /// while also being unsorted.
         /// </summary>
         /// <param name="item">Generic item type</param>
         /// <param name="priority">Priority of item</param>
@@ -63,12 +61,18 @@ namespace PriorityQueue
             }
             else
             {
+                // [(1,80) (2, 90) (3, 100)] FIFO
                 Node<T> currentNode = _head;
-                while (currentNode.Next != null)
+                while (currentNode.Next != null)    // While the Current Node does not have a NULL Next pointer, the last Node will
                 {
-                    currentNode = currentNode.Next;
+                    currentNode = currentNode.Next; // Traverse the LinkedList
                 }
-                currentNode.Next = newNode;
+                currentNode.Next = newNode;         // At the last Node of the LinkedList (with the NULL pointer) change its Next Pointer to
+                                                    // point to the newly created Node, adding the newNode to the end of the LinkedList
+
+                // [(3,100) (2, 90) (1, 80)] LIFO
+                //newNode.Next = _head;
+                //_head = newNode;
             }
         }
 
@@ -91,32 +95,80 @@ namespace PriorityQueue
             Node<T> highestPriorityNode = _head;
             Node<T> previousNode = null;
 
-            // Traverse the LinkedList to find the Node with the Highest Priority, starting at the Head Node
+            int count = 0;
+            int highestPriorityCount = 0;
+
+            // Find HighestPriority Node while traversing each node, recording the count of the Nodes and the Index of the HighestPriorityNode
             while (currentNode != null)
             {
-                if (currentNode.Priority > highestPriorityNode.Priority)    // Compare values
+                count++;                            // Increment count
+
+                if (currentNode.Priority > highestPriorityNode.Priority)
                 {
-                    highestPriorityNode = currentNode;                      // Update the new Highest Priority Node 
-                    previousNode = currentNode;
+                    highestPriorityNode = currentNode;
+                    highestPriorityCount = count;   // This is the Index of the Highest Priority Node
                 }
-                currentNode = currentNode.Next;                             // Move to the next Node
+                
+                currentNode = currentNode.Next;     // Move to next Node
             }
 
 
-            /* Removing the Highest Priority Node.
-            /  This jumps the link over the HighestPriorityNode because we change the Pointers,
-            /  we link the Previous Node to the one AFTER the HighestPriority using the
-            /  HighestPriorityNodes Next Pointer.   [Previous] [HighestPrio] [HighestPrioNext]
-            /                                           |____________________________|      
-            */
-            if (highestPriorityNode == _head)   // If the Node to be removed is the head
+            // Loop to find the PreviousNode to the HighestPriorityNode 
+            currentNode = _head;                // Reset the currentNode to the start of the LinkedList again, because of the While loop
+            for (int i = 1; currentNode != null; i++)
             {
-                _head = _head.Next;             // Change the pointer of the head to the next Node in the LinkedList
+                if (i == highestPriorityCount - 1)
+                {
+                    previousNode = currentNode; // This is the previousNode to the HighestPriorityNode
+                    break;
+                }
+
+                currentNode = currentNode.Next; // Move to next node
+            }
+
+
+            // Remove HighestPriorityNode from LinkedList
+            if (highestPriorityNode == _head)
+            {
+                _head = _head.Next;
             }
             else
             {
-                previousNode.Next = highestPriorityNode.Next;   // Change the PreviousNodes Pointer to HighestPriorityNodes Next Pointer.
-            }  
+                previousNode.Next = highestPriorityNode.Next;
+            }
+
+            //Node<T> currentNode = _head;
+            //Node<T> highestPriorityNode = _head;
+            //Node<T> previous = null;
+            //Node<T> previousNode = null;
+
+
+            //        // Traverse the LinkedList to find the Node with the Highest Priority, starting at the Head Node
+            //        while (currentNode != null)
+            //        {
+            //            if (currentNode.Priority > highestPriorityNode.Priority)    // If the CurrentNode Priority is greater than the record of the highestPriorityNode
+            //            {
+            //                highestPriorityNode = currentNode;                      // Highest Priority Node is this currentNode, since the currentNode has the higher Priority value   
+            //                previousNode = previous;                                // The previousNode is the previous value
+            //            }
+            //            previous = currentNode;                                     // Previous is the currentNode
+            //            currentNode = currentNode.Next;                             // Move to the next Node
+            //        }
+
+            //    /* Removing the Highest Priority Node.
+            //    /  This jumps the link over the HighestPriorityNode because we change the Pointers,
+            //    /  we link the PreviousNode to the one AFTER the HighestPriority using the
+            //    /  HighestPriorityNodes Next Pointer.   [Previous] [HighestPrio] [HighestPrioNext]
+            //    /                                           |____________________________|      
+            //    */
+            //    if (highestPriorityNode == _head)   // If the Node to be removed is the head
+            //    {
+            //        _head = _head.Next;             // Change the pointer of the head to the next Node in the LinkedList, this will remove the head node from the LinkedList
+            //    }
+            //    else
+            //    {
+            //        previousNode.Next = highestPriorityNode.Next;   // Change the PreviousNodes Pointer to HighestPriorityNodes Next Pointer.  
+            //    }
         }
 
 
@@ -158,7 +210,7 @@ namespace PriorityQueue
                 currentNode = currentNode.Next;                             // Move to the Next Node
             }
             result += "]";                  // Add a square bracket to the end
-            return result;
+            return result;                  // Return the concatenated result
         }
     }
 }
