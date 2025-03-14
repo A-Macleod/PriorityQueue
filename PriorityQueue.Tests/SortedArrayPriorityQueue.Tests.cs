e651ede9b5d61b89f11884eb1b5d3c888a1e0078
A-Macleod <0308180@uhi.ac.uk>
2025-03-14 21:06:33 +0000

    TEST Branch - SortedArrayPriorityQueue / Added Setup test method

--- PriorityQueue.Tests/SortedArrayPriorityQueue.Tests.cs
+++ PriorityQueue.Tests/SortedArrayPriorityQueue.Tests.cs
@@ -5,7 +5,15 @@ namespace PriorityQueue.Tests
 {
     public class SortedArrayPriorityQueueTests
     {
+        private SortedArrayPriorityQueue<Person> sortedArrayPriorityQueue;
 
+
+        [SetUp]
+        public void Setup()
+        {
+            int size = 8;
+            sortedArrayPriorityQueue = new SortedArrayPriorityQueue<Person>(size);
+        }
 
 
 
