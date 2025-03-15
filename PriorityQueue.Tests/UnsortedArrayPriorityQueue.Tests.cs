8dce8f21d04ae9972bab8cf4feaff1eb4a1d6dda
A-Macleod <0308180@uhi.ac.uk>
2025-03-15 19:23:22 +0000

    TEST Branch - UnsortedArrayPriorityQueue / Added Remove_AddsAndFillsTheQueueThenRemovesAllTheItemsFromTheQueueThenThrowsExceptionErrorWhenEmpty
    Asserts that when Dequeuing every element from a full queue works and Dequeuing from an Empty queue throws an UnderflowException error

--- PriorityQueue.Tests/UnsortedArrayPriorityQueue.Tests.cs
+++ PriorityQueue.Tests/UnsortedArrayPriorityQueue.Tests.cs
@@ -204,6 +204,34 @@ namespace PriorityQueue.Tests
 
 
 
+        [Test]
+        public void Remove_AddsAndFillsTheQueueThenRemovesAllTheItemsFromTheQueueThenThrowsExceptionErrorWhenEmpty()
+        {
+            // Arrange
+            UnsortedArrayPriorityQueue.Add(new Person("one"), 10);
+            UnsortedArrayPriorityQueue.Add(new Person("two"), 20);
+            UnsortedArrayPriorityQueue.Add(new Person("three"), 30);
+            UnsortedArrayPriorityQueue.Add(new Person("four"), 40);
+            UnsortedArrayPriorityQueue.Add(new Person("five"), 50);
+            UnsortedArrayPriorityQueue.Add(new Person("six"), 60);
+            UnsortedArrayPriorityQueue.Add(new Person("seven"), 70);
+            UnsortedArrayPriorityQueue.Add(new Person("eight"), 80);
+
+            // Act
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+            UnsortedArrayPriorityQueue.Remove();
+
+            // Assert
+            Assert.That(() => UnsortedArrayPriorityQueue.Remove(), Throws.TypeOf<QueueUnderflowException>());
+        }
+
+
 
 
 
