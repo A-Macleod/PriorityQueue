10f62a22d339d4708f824c859187b5a07044b813
A-Macleod <0308180@uhi.ac.uk>
2025-03-17 22:32:09 +0000

    TEST Branch - SortedLinkedPriorityQueue / Added Test file and created Setup and Teardown tests

---
+++ PriorityQueue.Tests/SortedLinkedPriorityQueue.Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace PriorityQueue.Tests
+{
+    public class SortedLinkedPriorityQueue
+    {
+
+        private SortedLinkedPriorityQueue<Person> sortedLinkedPriorityQueue;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            sortedLinkedPriorityQueue = new SortedLinkedPriorityQueue<Person>();
+        }
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            sortedLinkedPriorityQueue = null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
