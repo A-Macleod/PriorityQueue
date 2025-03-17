284471bb549baae88cc5b1b57f7138d0ec3d42d7
A-Macleod <0308180@uhi.ac.uk>
2025-03-17 17:16:58 +0000

    TEST Branch - UnsortedLinkedPriorityQueue / Added SetUp & TearDown

---
+++ PriorityQueue.Tests/UnsortedLinkedPriorityQueue.Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace PriorityQueue.Tests
+{
+    public class UnsortedLinkedPriorityQueue
+    {
+
+        private UnsortedLinkedPriorityQueue<Person> unsortedLinkedPriorityQueue;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            unsortedLinkedPriorityQueue = new UnsortedLinkedPriorityQueue<Person>();
+        }
+
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            unsortedLinkedPriorityQueue = null;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
