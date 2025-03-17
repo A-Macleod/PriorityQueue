c869a60f6e6057dd79769221d8d6c51f434d14b3
A-Macleod <0308180@uhi.ac.uk>
2025-03-17 17:23:18 +0000

    TEST Branch - UnsortedLinkedPriorityQueue / Added IsEmpty Test Methods
    IsEmpty_CheckTheQueueIsEmptyWhenQueueIsEmpty
    IsEmpty_CheckTheQueueIsNotEmptyWhenItemsAreAdded
    IsEmpty_CheckTheQueueIsNotEmptyWhenQueueIsFull

--- PriorityQueue.Tests/UnsortedLinkedPriorityQueue.Tests.cs
+++ PriorityQueue.Tests/UnsortedLinkedPriorityQueue.Tests.cs
@@ -31,6 +31,96 @@ namespace PriorityQueue.Tests
 
 
 
+        [Test]
+        public void IsEmpty_CheckTheQueueIsEmptyWhenQueueIsEmpty()
+        {
+            // Assert
+            Assert.That(unsortedLinkedPriorityQueue.IsEmpty, Is.True);
+        }
+
+
+
+        [Test]
+        public void IsEmpty_CheckTheQueueIsNotEmptyWhenItemsAreAdded()
+        {
+            // Act
+            unsortedLinkedPriorityQueue.Add(new Person("one"), 90);
+
+            // Assert
+            Assert.That(unsortedLinkedPriorityQueue.IsEmpty, Is.False);
+        }
+
+
+
+        [Test]
+        public void IsEmpty_CheckTheQueueIsNotEmptyWhenQueueIsFull()
+        {
+            // Act
+            unsortedLinkedPriorityQueue.Add(new Person("one"), 10);
+            unsortedLinkedPriorityQueue.Add(new Person("two"), 20);
+            unsortedLinkedPriorityQueue.Add(new Person("three"), 30);
+            unsortedLinkedPriorityQueue.Add(new Person("four"), 40);
+            unsortedLinkedPriorityQueue.Add(new Person("five"), 50);
+            unsortedLinkedPriorityQueue.Add(new Person("six"), 60);
+            unsortedLinkedPriorityQueue.Add(new Person("seven"), 70);
+            unsortedLinkedPriorityQueue.Add(new Person("eight"), 80);
+
+            // Assert
+            Assert.That(unsortedLinkedPriorityQueue.IsEmpty, Is.False);
+        }
+
+
+
+        [Test]
+        public void Add_AddingOneItemToQueue()
+        {
+            // Arrange
+            unsortedLinkedPriorityQueue.Add(new Person("one"), 10);
+
+            // Act
+            var head = unsortedLinkedPriorityQueue.Head();
+
+            // Assert
+            Assert.That("one", Is.EqualTo(head.Name));
+        }
+
+
+
+        [Test]
+        public void Add_AddingRemovingThenAddingWhileTrackingTheHighestPriority()
+        {
+            unsortedLinkedPriorityQueue.Add(new Person("one"), 10);
+            unsortedLinkedPriorityQueue.Add(new Person("four"), 40);
+            unsortedLinkedPriorityQueue.Add(new Person("two"), 20);
+            unsortedLinkedPriorityQueue.Add(new Person("three"), 30);
+
+            unsortedLinkedPriorityQueue.Remove();
+            unsortedLinkedPriorityQueue.Remove();
+            unsortedLinkedPriorityQueue.Remove();
+
+            unsortedLinkedPriorityQueue.Add(new Person("five"), 50);
+
+            var head = unsortedLinkedPriorityQueue.Head();
+
+            Assert.That("five", Is.EqualTo(head.Name));
+        }
+
+
+
+        [Test]
+        public void Add_AddingItemWithNullNameDoesNotGetAddedToQueue()
+        {
+            // Arrange
+            unsortedLinkedPriorityQueue.Add(new Person(null), 10);
+
+            // Act
+            var result = unsortedLinkedPriorityQueue.Head();
+
+            // Assert
+            Assert.That("[(, 10)]", Is.Not.EqualTo(result)); ;
+        }
+
+
 
 
 
